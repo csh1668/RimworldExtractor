b6113287a1eb06655a847831b13d8e6c0bdaa144
SeoHyeon <tjgus1668@naver.com>
2024-02-28 16:23:33 +0900

    delete unused files
    Update tools.py

--- tools.py
+++ tools.py
@@ -2,32 +2,55 @@ import sys
 from os import path
 import os
 import zipfile
+import codecs
 
 sys.stdout.reconfigure(encoding='utf-8')
 
 ROOT_PATH = os.getcwd()
 FILE_PATH = path.join("RimworldExtractorGUI", "Program.cs")
 PORTABLE_PATH = path.join("publish_standard", "RimworldExtractorGUI.exe")
+TEMPLATE_PATH = path.join('.github', 'release_template.txt')
 
 
-def get_current_version():
+def write_output(key, value):
+    if 'GITHUB_OUTPUT' in os.environ:
+        with open(os.environ['GITHUB_OUTPUT'], 'a', encoding='utf-8') as state_file:
+            state_file.write(f"{key}={value}\n")
+        print(f'GITHUB_OUTPUT: written {key}={value}')
+    else:
+        print(f'{key}={value}')
+
+
+def edit_template(changelog):
+    with open(TEMPLATE_PATH, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    lines.insert(0, changelog.strip())
+    lines.insert(1, '\n')
+    lines.insert(1, '\n')
+    with open(TEMPLATE_PATH, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
+
+def write_current_version(version):
     if not path.exists(FILE_PATH):
         print(f'no file exists in {FILE_PATH}')
         return False
     try:
-        with open(FILE_PATH, 'r') as file:
-            line = file.readline()
-            while line:
+        lines = []
+        with open(FILE_PATH, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            for (idx, line) in enumerate(lines):
                 if "string VERSION" in line:
                     start = line.index('"') + 1
-                    end = line.rindex('"')
-                    version = line[start:end]
-                    print(f"::set-output name=tag_name::{version}")
-                    return True
-                line = file.readline()
+                    lines[idx] = f'{lines[idx][:start]}{version}";'
+                    print(f'write_current_version done: {lines[idx]}')
+                    break
+        with open(FILE_PATH, 'w', encoding='utf-8') as file:
+            file.writelines(lines)
     except ValueError as err:
         print("ValueError {0}".format(err))
-    return False
+        return False
+    return True
 
 
 def cleanup_standard():
@@ -45,7 +68,7 @@ def cleanup_standard():
     os.remove(path.join(standard_path, "RimworldExtractorGUI.deps.json"))
     os.remove(path.join(standard_path, "RimworldExtractorInternal.pdb"))
 
-    zip_filename = path.join(ROOT_PATH, 'standard.zip')
+    zip_filename = path.join(ROOT_PATH, 'RimworldExtractor-Standard.zip')
     with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, _, files in os.walk(standard_path):
             for file in files:
@@ -56,15 +79,40 @@ def cleanup_standard():
 def cleanup_portable():
     portable_path = "publish_portable"
     print(path.exists(path.join(portable_path, 'RimworldExtractorGUI.exe')))
-    os.rename(path.join(portable_path, 'RimworldExtractorGUI.exe'), path.join(ROOT_PATH, 'portable.exe'))
-    print(path.join(ROOT_PATH, 'portable.exe'))
+    new_path = path.join(ROOT_PATH, 'RimworldExtractor-Portable.exe')
+    os.rename(path.join(portable_path, 'RimworldExtractorGUI.exe'), new_path)
+    print(new_path)
+
+
+def convert_utf8(file_path):
+    print(f'convert_utf8: {file_path}')
+    encodings_to_try = ['utf-8', 'cp949']
+    for encoding in encodings_to_try:
+        try:
+            with codecs.open(file_path, 'r', encoding=encoding) as f:
+                content = f.read()
+            with codecs.open(file_path, 'w', encoding='utf-8') as f:
+                f.write(content)
+            print(f"Successfully converted from {encoding} to UTF-8.")
+            return
+        except UnicodeDecodeError as err:
+            print(f"Failed to decode with {encoding}: {err}")
+
+
+def ensure_utf8():
+    for root, _, files in os.walk(ROOT_PATH):
+        for file in files:
+            full_path = path.join(root, file)
+            if not file.endswith('.cs'):
+                continue
+            convert_utf8(full_path)
 
 
 if __name__ == "__main__":
     print(ROOT_PATH)
     command = sys.argv[1]
     if command == 'version':
-        result = get_current_version()
+        result = write_current_version(sys.argv[2])
         if result is True:
             quit(0)
         else:
@@ -72,3 +120,7 @@ if __name__ == "__main__":
     elif command == 'cleanup':
         cleanup_standard()
         cleanup_portable()
+    elif command == 'utf8':
+        ensure_utf8()
+    elif command == 'template':
+        edit_template(sys.argv[2])
