442fd7990755208c261a3abb3cc4ec786ec9da5a
SeoHyeon <tjgus1668@naver.com>
2024-06-08 13:52:37 +0900

    feat: 미리 정의된 FactionDef 속성 #20

---
+++ RimworldExtractorInternal/Compats/Compat_FactionDef.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RimworldExtractorInternal.Compats
+{
+    internal class Compat_FactionDef : BaseCompat
+    {
+        private const string selector =
+            "Defs/FactionDef";
+        public override void DoPreProcessing(XmlDocument doc)
+        {
+            var nodes = doc.SelectNodesSafe(selector);
+            if (nodes == null)
+                return;
+            foreach (XmlNode node in nodes)
+            {
+                var pawnSingular = node["pawnSingular"];
+                var pawnsPlural = node["pawnsPlural"];
+                var leaderTitle = node["leaderTitle"];
+
+                if (pawnSingular == null)
+                    node.AppendElement("pawnSingular", "member");
+                if (pawnsPlural == null)
+                    node.AppendElement("pawnsPlural", "members");
+                if (leaderTitle == null)
+                    node.AppendElement("leaderTitle", "leader");
+            }
+        }
+    }
+}
