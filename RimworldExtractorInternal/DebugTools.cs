30507bb04cdf61832d7036b87451729155d5ece0
SeoHyeon <tjgus1668@naver.com>
2025-01-03 15:41:05 +0900

    fix: 어반루인 모드 추출 문제 해결 fix: 타겟 프레임워크 변경 .NET 7.0 -> .NET 9.0 fix: `Extractor.DefsUtils.cs` 파일의 `GetIdxOfListNode` 메서드에 노드 자식의 개수가 1000개를 초과할 경우 경고 메시지를 로그로 남기는 기능을 추가

---
+++ RimworldExtractorInternal/DebugTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RimworldExtractorInternal
+{
+    public static class DebugTools
+    {
+        public static void DumpXml(XmlDocument doc)
+        {
+            const string path = "dump.xml";
+            doc.Save(path);
+
+            //Process.Start(path);
+        }
+
+        public static void DumpXml(XmlNode node)
+        {
+            const string path = "dump.xml";
+            var doc = new XmlDocument();
+            doc.AppendChild(doc.ImportNode(node, true));
+            doc.Save(path);
+
+            //Process.Start(path);
+        }
+    }
+}
